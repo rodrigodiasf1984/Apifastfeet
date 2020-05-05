affc8144af3cb9b818b6b9059fc1c5d0395cd1c7
Rodrigo <rodrigodiasf@gmail.com>
2020-05-05 18:18:38 +0100

    Roobacl UpdateDeliveryService

--- src/app/controllers/DeliveryController.js
+++ src/app/controllers/DeliveryController.js
@@ -1,3 +1,4 @@
+import { parseISO, getHours, isBefore } from 'date-fns';
 import { Op } from 'sequelize';
 import File from '../models/File';
 import Delivery from '../models/Delivery';
@@ -5,7 +6,6 @@ import Recipient from '../models/Recipient';
 import Deliveryman from '../models/Deliveryman';
 import RegistrationMail from '../jobs/RegistrationMail';
 import Queue from '../../lib/Queue';
-import UpdateDeliveryService from '../services/UpdateDeliveryService';
 
 class DeliveryController {
   async store(req, res) {
@@ -61,26 +61,87 @@ class DeliveryController {
   }
 
   async update(req, res) {
+    const { id } = req.params;
+    const delivery = await Delivery.findByPk(id);
+
+    if (!delivery)
+      return res.status(400).json({ error: 'Delivery does not exists' });
+
     const {
-      product,
       deliveryman_id,
       recipient_id,
       signature_id,
       start_date,
       end_date,
     } = req.body;
+    if (deliveryman_id && deliveryman_id !== delivery.deliveryman_id) {
+      const deliveryman = await Deliveryman.findByPk(deliveryman_id);
+      if (!deliveryman) {
+        return res.status(400).json({ error: 'Deliveryman does not exists.' });
+      }
+    }
 
-    const { id } = req.params;
+    if (recipient_id && recipient_id !== delivery.recipient_id) {
+      const recipient = await Recipient.findByPk(recipient_id);
+      if (!recipient) {
+        return res.status(400).json({ error: 'Recipient does not exists.' });
+      }
+    }
 
-    const deliveryUpdated = await UpdateDeliveryService.run({
-      product,
-      deliveryman_id,
-      recipient_id,
-      signature_id,
-      start_date,
-      end_date,
-      delivery_id: id,
-    });
+    if (signature_id && signature_id !== delivery.signature_id) {
+      const file = await File.findByPk(signature_id);
+      if (!file) {
+        return res.status(400).json({ error: 'File does not exists.' });
+      }
+    }
+    // verifica se as horas quando forem alteradas, se as mesma estão entre 08:00 e  18:00
+
+    const parsedStartDate = parseISO(start_date);
+    const parsedEndDate = parseISO(end_date);
+    // verifica se o utilizador não está inserindo uma data de inicio ou fim da entrega antes da data atual
+    if (
+      isBefore(parsedStartDate, new Date()) ||
+      isBefore(parsedEndDate, new Date())
+    ) {
+      return res.status(400).json({
+        error: "The start date or end date can not be before today's date",
+      });
+    }
+    if (parsedStartDate) {
+      const startHour = getHours(parsedStartDate);
+      if (startHour <= 8 || startHour >= 18) {
+        return res
+          .status(400)
+          .json({ error: 'The start hour must be between 08:00 and 18:00' });
+      }
+    }
+    if (parsedEndDate) {
+      const startHour = getHours(parsedStartDate);
+      if (startHour <= 8 || startHour >= 18) {
+        return res
+          .status(400)
+          .json({ error: 'The start hour must be between 08:00 and 18:00' });
+      }
+    }
+
+    if (end_date && !start_date) {
+      if (!delivery.start_date) {
+        return res
+          .status(400)
+          .json({ error: 'The delivery must have a start date!' });
+      }
+    }
+
+    if (start_date && end_date) {
+      if (isBefore(parsedEndDate, parsedStartDate)) {
+        return res
+          .status(400)
+          .json({ error: 'The end date can not be before the start date!' });
+      }
+    }
+
+    const deliveryUpdated = await delivery.update(req.body);
+
     return res.json({ deliveryUpdated });
   }
 
